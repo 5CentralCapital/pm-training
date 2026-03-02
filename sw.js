ab9131ebd9f5b652039ca5b8b72783faabd73631
5CentralCapital <michael@5central.capital>
2026-03-01 22:26:18 -0500

    Deploy PM Training app

---
+++ sw.js
@@ -0,0 +1,28 @@
+// IMPORTANT: Bump version number every time you deploy changes (e.g., v3, v4...)
+const CACHE_NAME = '5c-training-v3';
+const ASSETS = [
+  './5Central_PM_Training.html',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Playfair+Display:wght@400;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
